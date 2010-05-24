779cdbebdb88e8fbf3bce9fe230e99f7e54e21d8
intraface <support@intraface.dk>
2010-05-24 08:56:39 -0700

    Fixed code according to salathe's suggestion on http://www.sitepoint.com/forums/showthread.php?t=678582

--- src/Ilib/Date.php
+++ src/Ilib/Date.php
@@ -61,24 +61,22 @@ class Ilib_Date
         $d = "([0-3]?[0-9])";
         $m = "([0-1]?[0-9])";
         $y = "([0-9][0-9][0-9][0-9]|[0-9]?[0-9])";
-      //$s = "(-|\.|/| )";
-        $s = "(-|\.|\/| )";
+        $s = "[-\.\/\s]";
 
         if ($default_year == "") {
             $default_year = date("Y");
         }
 
-
         if (preg_match("/^".$d.$s.$m.$s.$y."$/", $this->date, $parts)) {
             // true
         } elseif(preg_match("/^".$d.$s.$m."$/", $this->date, $parts)) {
-            $parts[5] = $default_year;
+            $parts[3] = $default_year;
             // true
         } else {
             return false;
         }
 
-        $this->date = $parts[5]."-".$parts[3]."-".$parts[1];
+        $this->date = $parts[3]."-".$parts[2]."-".$parts[1];
         return true;
     }
 
